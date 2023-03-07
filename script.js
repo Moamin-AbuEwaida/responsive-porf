47aa9698297c439381703c5c8f15ba1b42e4d64c
Moamin <moamin.salamah@gmail.com>
2023-03-07 11:49:14 +0100

    navbar behavior with small screens

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// menu icon toggling behavior
+let menuIcon = document.querySelector('#menu-icon');
+let  navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+// scrolling behavior 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -18,4 +27,7 @@ window.onscroll= ()=>{
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+     menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
